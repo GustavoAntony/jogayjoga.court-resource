b2121d13e2f5ab3c131403b90fdf7013c591cea6
GustavoAntony <gustavoa9@al.insper.edu.br>
2024-05-24 21:31:52 -0300

    inetger no sql script

--- target/classes/db/migration/V2024.02.27.002__create_table_account.sql
+++ target/classes/db/migration/V2024.02.27.002__create_table_account.sql
@@ -5,5 +5,6 @@ CREATE TABLE court
     tx_address character varying(256) NOT NULL,
     tx_capacity INTEGER NOT NULL,
     id_sport character varying(256) NOT NULL,
+    tx_reserved INTEGER,
     CONSTRAINT pk_court PRIMARY KEY (id_court)
 );
